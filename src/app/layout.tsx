956c6644d8576658b9f2bf1b9a064f66eaf2c126
Augustine Edeh <info.augustinesedeh@gmail.com>
2026-04-10 12:12:09 +0100

    chore: add login UI and composing components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/lib/queryClient";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -40,6 +40,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <QueryClientProvider client={queryClient}>
           {children}
+          <Toaster position="top-right" richColors />
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </body>
