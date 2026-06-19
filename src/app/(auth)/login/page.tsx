839606109249f23a997145b5aca958dfde7e9151
Augustine Edeh <info.augustinesedeh@gmail.com>
2026-06-19 01:49:54 +0100

    chore: add back button to all auth pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,25 @@
 import { Suspense } from "react";
 import LoginForm from "./LoginForm";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <Suspense>
-      <LoginForm />
+      <div className="relative">
+        <Link
+          href="/"
+          title="back to home"
+          className="absolute top-4 left-4 group inline-flex items-center gap-1 text-slate-500 hover:text-slate-300 text-sm transition-colors"
+        >
+          <div className="group-hover:bg-slate-900 p-2 rounded-full">
+            <ChevronLeft size={14} />
+          </div>
+          <span className="hidden sm:block">Back to home</span>
+        </Link>
+
+        <LoginForm />
+      </div>
     </Suspense>
   );
 }
